0d7f9c7104d745f6f2cadfc3e8455239c5d68fd6
alfill <7522884+alfill@users.noreply.github.com>
2019-12-13 21:57:49 +0500

    remove unnecessary files

--- precache-manifest.149c4d71ecd5cb789692d74cb9fccb7a.js
+++
@@ -1,9 +0,0 @@
-self.__precacheManifest = [
-  {
-    "url": "main.961f7cc0a079f1755455.bundle.js"
-  },
-  {
-    "revision": "2d441745d5b8bca5c36d",
-    "url": "styles.css"
-  }
-];
--- precache-manifest.95b90ed20476398228303071ee306f9e.js
+++
@@ -1,9 +0,0 @@
-self.__precacheManifest = [
-  {
-    "revision": "ff6047a50c18ee526f10",
-    "url": "styles.css"
-  },
-  {
-    "url": "main.201113f887d44acdfa77.bundle.js"
-  }
-];
