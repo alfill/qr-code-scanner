9a3aa24608b6d4f19bb76bc46854131171f9850e
alfill <alfill@users.noreply.github.com>
2023-02-12 18:12:11 +0000

    Deploying to gh-pages from  @ 4dcc062ad73b857b935c6418475fe4ebd1e1aec2 🚀

---
+++ precache-manifest.48f85e56087e547447725b41f1f2d7ce.js
@@ -0,0 +1,9 @@
+self.__precacheManifest = [
+  {
+    "url": "main.90c9a0e5e9841f202ff6.bundle.js"
+  },
+  {
+    "revision": "d1346f3f8de0c1cb039f",
+    "url": "styles.css"
+  }
+];
--- precache-manifest.e222b3cf201ff8065901f577ddd3efc0.js
+++
@@ -1,9 +0,0 @@
-self.__precacheManifest = [
-  {
-    "url": "main.05316bba3ad29c00b4a3.bundle.js"
-  },
-  {
-    "revision": "44cbca12472dff212fcd",
-    "url": "styles.css"
-  }
-];
